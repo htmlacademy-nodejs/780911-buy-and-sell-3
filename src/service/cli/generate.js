d05999d3250ab143175112625a3b792021943502
Olga Shitova <olgashitova@MacBook-Pro-2.local>
2021-01-19 22:37:06 +0200

    started to do a task

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,44 +1,47 @@
 'use strict';
 
 const fs = require(`fs`).promises;
+const path = require(`path`);
 const {
   getRandomInt,
   shuffle,
 } = require(`../../utils`);
 const chalk = require(`chalk`);
-
+const titles = `../../../data/titles.txt`;
+const sentences = `../../../data/sentences.txt`;
+const categories = `../../../data/categories.txt`;
 const DEFAULT_COUNT = 1;
 const FILE_NAME = `mocks.json`;
 
-const TITLES = [
-  `Продам книги Стивена Кинга`,
-  `Продам новую приставку Sony Playstation 5`,
-  `Продам отличную подборку фильмов на VHS`,
-  `Куплю антиквариат`,
-  `Куплю породистого кота`,
-];
-
-const SENTENCES = [
-  `Товар в отличном состоянии.`,
-  `Пользовались бережно и только по большим праздникам.`,
-  `Продаю с болью в сердце...`,
-  `Бонусом отдам все аксессуары.`,
-  `Даю недельную гарантию.`,
-  `Если товар не понравится — верну всё до последней копейки.`,
-  `Это настоящая находка для коллекционера!`,
-  `Если найдёте дешевле — сброшу цену.`,
-  `Таких предложений больше нет!`,
-  `При покупке с меня бесплатная доставка в черте города.`,
-];
-
-const CATEGORIES = [
-  `Книги`,
-  `Разное`,
-  `Посуда`,
-  `Игры`,
-  `Животные`,
-  `Журналы`,
-];
+// const TITLES = [
+//   `Продам книги Стивена Кинга`,
+//   `Продам новую приставку Sony Playstation 5`,
+//   `Продам отличную подборку фильмов на VHS`,
+//   `Куплю антиквариат`,
+//   `Куплю породистого кота`,
+// ];
+//
+// const SENTENCES = [
+//   `Товар в отличном состоянии.`,
+//   `Пользовались бережно и только по большим праздникам.`,
+//   `Продаю с болью в сердце...`,
+//   `Бонусом отдам все аксессуары.`,
+//   `Даю недельную гарантию.`,
+//   `Если товар не понравится — верну всё до последней копейки.`,
+//   `Это настоящая находка для коллекционера!`,
+//   `Если найдёте дешевле — сброшу цену.`,
+//   `Таких предложений больше нет!`,
+//   `При покупке с меня бесплатная доставка в черте города.`,
+// ];
+//
+// const CATEGORIES = [
+//   `Книги`,
+//   `Разное`,
+//   `Посуда`,
+//   `Игры`,
+//   `Животные`,
+//   `Журналы`,
+// ];
 
 const OfferType = {
   OFFER: `offer`,
@@ -68,24 +71,33 @@ const getPictureFileName = (num1, num2) => {
   return `item${zeroFill(getRandomInt(num1, num2), 2)}.jpg`;
 };
 
-const generateOffers = (count) => (
-  Array(count).fill({}).map(() => ({
-    category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]],
-    description: shuffle(SENTENCES).slice(1, 5).join(` `),
-    picture: getPictureFileName(getRandomInt(PictureRestrict.MIN, PictureRestrict.MAX), 2),
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
-    type: Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)],
-    sum: getRandomInt(SumRestrict.MIN, SumRestrict.MAX),
-  }))
-);
+async function readFile(fileName) {
+    try {
+      const file = fs.readFile(path.resolve(__dirname, fileName), `utf-8`);
+      return file;
 
+    } catch (e) {
+      console.log("e", e);
+    }
+}
+
+// const generateOffers = (count) => (
+//   Array(count).fill({}).map(() => ({
+//     category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]],
+//     description: shuffle(SENTENCES).slice(1, 5).join(` `),
+//     picture: getPictureFileName(getRandomInt(PictureRestrict.MIN, PictureRestrict.MAX), 2),
+//     title: TITLES[getRandomInt(0, TITLES.length - 1)],
+//     type: Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)],
+//     sum: getRandomInt(SumRestrict.MIN, SumRestrict.MAX),
+//   }))
+// );
 
 module.exports = {
   name: `--generate`,
   async run(args) {
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    const content = JSON.stringify(generateOffers(countOffer));
+    //const content = JSON.stringify(generateOffers(countOffer));
 
     if (countOffer > 999) {
       console.log(chalk.red(`Не больше 1000 объявлений`));
@@ -93,7 +105,9 @@ module.exports = {
     }
 
     try {
-      await fs.writeFile(FILE_NAME, JSON.stringify(content));
+      const res = await readFile(titles);
+      console.log(res.split("\n"));
+      await fs.writeFile(FILE_NAME, JSON.stringify('content'));
       console.log(chalk.green(`Operation success. File created.`));
     } catch (error) {
       console.error(chalk.red(`Can't write data to file...`));
